1d0add48e69e7a029ae2770be826ec2d51081151
LewisJones2021 <81636428+LewisJones2021@users.noreply.github.com>
2023-08-30 20:46:46 +0100

    delete api enpoint added

--- api/api.go
+++ api/api.go
@@ -61,7 +61,7 @@ func ApiRoutes() error {
 	// router.GET("/api/houses/:id", getHouseByID)
 	router.POST("/api/houses", addHouse)
 	// router.PUT("/api/houses/:id", updateHouse)
-	// router.DELETE("/api/houses/:id", deleteHouse)
+	 router.DELETE("/api/houses/:id", deleteHouse)
 
 	// start the server.
 	if err := router.Run(":8080"); err != nil {
@@ -117,7 +117,7 @@ for cursor.Next(context.Background()){
 	c.JSON(http.StatusOK, data)
 }
 
-// function to create a new house and add to the da	tabase.
+// function to create a new house and add to the database.
 func addHouse(c* gin.Context) { 
 	var newHouse House
 if err := c.ShouldBindJSON(&newHouse); err != nil {
@@ -137,3 +137,39 @@ if err != nil{
 insertedID := result.InsertedID.(primitive.ObjectID).Hex()
 c.JSON(http.StatusCreated, gin.H{"message":"House successfully created.", "id": insertedID})
 }
+
+// function to delete the house from the database.
+
+// handle DELETE request to delete a house by its ID
+func deleteHouse (c *gin.Context){ 
+	// get the house id from the url param.
+	houseID := c.Param("id")
+
+	// convert the house id to a primitive.objectID.
+	objID, err := primitive.ObjectIDFromHex(houseID)
+
+if err != nil {
+	c.JSON(http.StatusBadRequest, gin.H{"error":"invalid house ID"})
+	return
+	}
+
+		// construct a filter to find the house by its ID
+		filter := bson.M{"_id": objID}
+
+		// delete the house from the data collection.
+		result, err := housesCollection.DeleteOne(context.Background(), filter)
+
+		if err !=nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"Failed to delete the house from the system"})
+			return
+		}
+
+		if result.DeletedCount == 0{
+			c.JSON(http.StatusNotFound, gin.H{"error":"House not found"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message":"House successfully deleted"})
+}
+
+
